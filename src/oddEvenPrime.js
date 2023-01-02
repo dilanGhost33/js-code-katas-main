2ae3c2f057e5b22c23c3b318927fe1be13b17286
Dilan Ramirez <drramirezc@utn.edu.ec>
2023-01-02 00:59:27 -0500

    prueba fin

--- src/oddEvenPrime.js
+++ src/oddEvenPrime.js
@@ -27,12 +27,10 @@ const isPrime = (numero) => {
       for (let x = 2; x < numero / 2; x++) {
           if (numero % x == 0) return false;
       }
-      // Si no se pudo dividir por ninguno de los de arriba, sí es primo
       return true;
 }
 
 const isOdd = (numero) => {
-    // Casos especiales
       if(numero % 2 == 0){
         return true
       }
